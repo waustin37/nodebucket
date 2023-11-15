0740d383fabccede9428036dd0474239939164d9
William John Austin <wiaustin@my365.bellevue.edu>
2023-11-15 14:12:56 -0800

    Trying to Address Deployment Issues

--- src/app/sign-in.service.ts
+++ src/app/sign-in.service.ts
@@ -19,13 +19,21 @@ export class SignInService {
   constructor(private http: HttpClient, private cookieService: CookieService) {}
 
   async findEmployeeById(employeeId: string): Promise<void> {
-    const response = await fetch(`https://localhost:10000/api/employees/${employeeId}`)
+    const response = await fetch(`http://localhost:3000/api/employees/${employeeId}`)
     const employee = await response.json()
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch employee data. Status: ${response.status}`);
+    }
 
     this.cookieService.set('session_user', employeeId.toString(), 1);
     this.cookieService.set('name', employee.firstName);
 
     this.employee = employee; // Set the fetched employee data into the service variable
+  }
+
+  catch (error) {
+    console.error('Error fetching employee data:', error);
   }
 
   validateEmployee(employeeId: string): boolean {
